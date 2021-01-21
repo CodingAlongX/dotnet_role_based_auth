30a2c886a29788e52c82edf970b164f27fbddf8a
S. M. JAHANGIR <smjxpro@gmail.com>
2021-01-21 20:14:48 +0600

    configured identity options
    Signed-off-by: S. M. JAHANGIR <smjxpro@gmail.com>

--- Startup.cs
+++ Startup.cs
@@ -35,6 +35,14 @@ namespace RoleBasedAuth
             });
             services.AddIdentity<AppUser, IdentityRole>()
                 .AddEntityFrameworkStores<AuthContext>();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
