86a78d17a46e4b4c55cfbc30bf7a6ce87981fc01
S. M. JAHANGIR <smjxpro@gmail.com>
2021-02-04 21:58:20 +0600

    added role based auth test endpoints
    Signed-off-by: S. M. JAHANGIR <smjxpro@gmail.com>

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -8,15 +8,35 @@ namespace RoleBasedAuth.Controllers
     [Route("api/[controller]")]
     public class TestController : ControllerBase
     {
-        [HttpGet]
-        //User has to be either Admin or Author
-        // [AuthorizeRoles(Role.Admin,Role.Author)]
-        // User has to be both Admin and Author
+        [HttpGet("for-admin")]
         [AuthorizeRoles(Role.Admin)]
-        // [AuthorizeRoles(Role.Author)]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetForAdmin()
         {
-            return Ok(new {message = "Hi"});
+            return Ok(new {message = "Hi! I guess you are an admin"});
+        }
+
+        [HttpGet("for-author")]
+        [AuthorizeRoles(Role.Author)]
+        public async Task<IActionResult> GetForAuthor()
+        {
+            return Ok(new {message = "Hi! I guess you are an author"});
+        }
+
+        [HttpGet("for-author-and-admin")]
+        // User has to be both Admin and Author
+        [AuthorizeRoles(Role.Author)]
+        [AuthorizeRoles(Role.Admin)]
+        public async Task<IActionResult> GetForAuthorAndAdmin()
+        {
+            return Ok(new {message = "Hi! I guess you are both author and admin"});
+        }
+
+        [HttpGet("for-author-or-admin")]
+        //User has to be either Admin or Author
+        [AuthorizeRoles(Role.Author, Role.Admin)]
+        public async Task<IActionResult> GetForAuthorOrAdmin()
+        {
+            return Ok(new {message = "Hi! I guess you are both author and admin"});
         }
     }
 }
